8fbcb2ec4dc0775f83bec5cbb56b99ecb2568320
汤志锋 <tangzhifeng@tangzhifengdemacbook-pro.local>
2019-04-29 22:05:56 +0800

    Initial Commit

---
+++ ZFCustomSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZFCustomSlider
+//
+//  Created by 汤志锋 on 2019/4/29.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
