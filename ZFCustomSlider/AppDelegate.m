88040e97adf7a6b3a5ec87e7467927c8efd7ca60
汤志锋 <tangzhifeng@tangzhifengdeMacBook-Pro.local>
2019-06-26 21:30:59 +0800

    1111

--- ZFCustomSlider/AppDelegate.m
+++ ZFCustomSlider/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"3333");
     return YES;
 }
 
